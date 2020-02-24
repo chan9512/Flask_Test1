d73045f23fd91086185ab612d891156dbe203425
chan9512 <59447235+chan9512@users.noreply.github.com>
2020-02-24 17:42:02 +0900

    Add files via upload

---
+++ Untitled3.py
@@ -0,0 +1,605 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[44]:
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return 'Hello Flask'
+
+if __name__ == '__main__':
+	app.run()
+
+import librosa
+import librosa.display
+import pyaudio #마이크를 사용하기 위한 라이브러리
+import wave
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+from sklearn.linear_model import LogisticRegression#텐서플로우로 바꿀예정
+import os
+##### 변수 설정 부분 #####
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100 #비트레이트 설정
+CHUNK = int(RATE / 10) # 버퍼 사이즈 1초당 44100비트레이트 이므로 100ms단위
+RECORD_SECONDS = 1 #녹음할 시간 설정
+WAVE_OUTPUT_FILENAME = "output.wav"
+DATA_PATH = "C:\\Users\\User\\Desktop\\바탕화면\\Downloads\\Speaker-Recognition-using-NN-master\\data"
+train_data=[]#train_date 저장할 공강
+train_label=[]#train_label 저장할 
+test_data=[]#train_date 저장할 공강
+test_label=[]#train_label 저장할 
+##########################
+
+def load_wave_generator(path): 
+       
+    batch_waves = []
+    labels = []
+    # input_width=CHUNK*6 # wow, big!!
+    folders = os.listdir(path)
+    #while True:
+       # print("loaded batch of %d files" % len(files))
+    for folder in folders:
+        if not os.path.isdir(path):continue #폴더가 아니면 continue                   
+        files = os.listdir(path+"/"+folder)        
+        print("Foldername :",folder,"-",len(files))#폴더 이름과 그 폴더에 속하는 파일 갯수 출력
+        for wav in files:
+            if not wav.endswith(".wav"):continue
+            else:
+                global train_data,train_label#전역변수를 사용하겠다.
+                print("Filename :",wav)#.wav 파일이 아니면 continue
+                y, sr = librosa.load(path+"/"+folder+"/"+wav)
+                mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13, hop_length=int(sr*0.01),n_fft=int(sr*0.02)).T
+                if(len(train_data)==0):
+                    train_data = mfcc
+                    train_label = np.full(len(mfcc), int(folder))
+                else:
+                    train_data = np.concatenate((train_data, mfcc), axis = 0)
+                    train_label = np.concatenate((train_label, np.full(len(mfcc),  int(folder))), axis = 0)
+                    #print("mfcc :",mfcc.shape)
+                
+load_wave_generator(DATA_PATH)
+
+
+######## 음성 데이터를 녹음 해 저장하는 부분 ########
+
+p = pyaudio.PyAudio() # 오디오 객체 생성
+
+stream = p.open(format=FORMAT, # 16비트 포맷
+                channels=CHANNELS, #  모노로 마이크 열기
+                rate=RATE, #비트레이트
+                input=True,
+                frames_per_buffer=CHUNK) # CHUNK만큼 버퍼가 쌓인다.
+
+print("Start to record the audio.")
+
+frames = [] # 음성 데이터를 채우는 공간
+
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)): 
+    #지정한  100ms를 몇번 호출할 것인지 10 * 5 = 50  100ms 버퍼 50번채움 = 5초
+    data = stream.read(CHUNK)
+    frames.append(data)
+
+print("Recording is finished.")
+
+stream.stop_stream() # 스트림닫기
+stream.close() # 스트림 종료
+p.terminate() # 오디오객체 종료
+
+# WAVE_OUTPUT_FILENAME의 파일을 열고 데이터를 쓴다.
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb') 
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
+
+spf = wave.open(WAVE_OUTPUT_FILENAME,'r')
+
+signal = spf.readframes(-1)
+signal = np.fromstring(signal, dtype=np.int16)
+
+#시간 흐름에 따른 그래프를 그리기 위한 부분
+Time = np.linspace(0,len(signal)/RATE, num=len(signal)) 
+
+plt.figure(1)
+plt.title('Voice Signal Wave...')
+#plt.plot(signal) // 음성 데이터의 그래프
+plt.plot(Time, signal)
+plt.show()
+
+
+######## 음성 데이터를 읽어와 학습 시키는 부분 ########
+
+print("train_data.shape :", train_data.shape, type(train_data))
+print("train_label.shape :", train_label.shape, type(train_label))
+#print(mfcc[0])
+#print(train_label)
+clf = LogisticRegression()
+clf.fit(train_data,train_label)
+
+y, sr = librosa.load("C:\\Users\\User\\Desktop\\바탕화면\\Downloads\\Speaker-Recognition-using-NN-master\\test_리찬희1.wav")
+#y, sr = librosa.load("./baecheolsu15.wav")
+plt.figure(figsize=(14,5))
+librosa.display.waveplot(y, sr)
+#y, sr = librosa.load(WAVE_OUTPUT_FILENAME)
+mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13, hop_length=int(sr*0.01),n_fft=int(sr*0.02)).T
+
+y_test_estimated = clf.predict(mfcc)
+print(y_test_estimated)
+test_label = np.full(len(mfcc), 5)
+print(test_label)
+'''
+0 유인나
+1 배철수
+2 이재은
+3 최일구
+4 문재인 대통령
+5 리찬희
+'''
+# 정답률 구하기 
+ac_score = metrics.accuracy_score(y_test_estimated, test_label)
+print("정답률 =", ac_score)
+print(pd.value_counts(pd.Series(y_test_estimated)))
+
+
+# In[19]:
+
+
+import pyaudio #마이크를 사용하기 위한 라이브러리
+import wave #.wav 파일을 저장하기 위한 라이브러리
+import os
+######## 음성 데이터를 녹음 해 저장하는 부분 ########
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100 #비트레이트 설정
+CHUNK = int(RATE / 10) # 버퍼 사이즈 1초당 44100비트레이트 이므로 100ms단위
+RECORD_SECONDS = 5 #녹음할 시간 설정
+#WAVE_OUTPUT_FILENAME = "./data/train/1/baecheolsu15.wav"
+WAVE_OUTPUT_FILENAME = "C:\\Users\\User\\Desktop\\바탕화면\\Downloads\\Speaker-Recognition-using-NN-master\\data\\5\\"
+FILE_NAME = "리찬희"
+
+files = os.listdir(WAVE_OUTPUT_FILENAME)
+wave_count = 1;
+     #폴더 이름과 그 폴더에 속하는 파일 갯수 출력
+for wav in files: 
+    if not wav.endswith(".wav"):continue
+    else: wave_count = wave_count+1
+
+
+WAVE_OUTPUT_FILENAME = WAVE_OUTPUT_FILENAME+FILE_NAME+str(wave_count)+".wav"
+print(str(wave_count)+"개의 .wav존재!",WAVE_OUTPUT_FILENAME)
+p = pyaudio.PyAudio() # 오디오 객체 생성
+
+stream = p.open(format=FORMAT, # 16비트 포맷
+                channels=CHANNELS, #  모노로 마이크 열기
+                rate=RATE, #비트레이트
+                input=True,
+                frames_per_buffer=CHUNK) # CHUNK만큼 버퍼가 쌓인다.
+
+print("Start to record the audio.")
+
+frames = [] # 음성 데이터를 채우는 공간
+
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)): 
+    #지정한  100ms를 몇번 호출할 것인지 10 * 5 = 50  100ms 버퍼 50번채움 = 5초
+    data = stream.read(CHUNK)
+    frames.append(data)
+
+print("Recording is finished.")
+
+stream.stop_stream() # 스트림닫기
+stream.close() # 스트림 종료
+p.terminate() # 오디오객체 종료
+
+wf = wave.open( WAVE_OUTPUT_FILENAME, 'wb') 
+# WAVE_OUTPUT_FILENAME의 파일을 열고 데이터를 쓴다.
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
+
+
+# In[45]:
+
+
+import librosa
+import pyaudio #마이크를 사용하기 위한 라이브러리
+import wave
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression#텐서플로우로 바꿀예정
+import os
+##### 변수 설정 부분 #####
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100 #비트레이트 설정
+CHUNK = int(RATE / 10) # 버퍼 사이즈 1초당 44100비트레이트 이므로 100ms단위
+RECORD_SECONDS = 5 #녹음할 시간 설정
+WAVE_OUTPUT_FILENAME = "output.wav"
+DATA_PATH = "C:\\Users\\User\\Desktop\\바탕화면\\Downloads\\Speaker-Recognition-using-NN-master\\data"
+X_train = []#train_data 저장할 공간
+X_test = []
+Y_train = []
+Y_test = []
+def load_wave_generator(path): 
+       
+    batch_waves = []
+    labels = []
+    X_data = []
+    Y_label = []
+    idx = 0
+    global X_train, X_test, Y_train, Y_test
+    folders = os.listdir(path)
+
+    for folder in folders:
+        if not os.path.isdir(path):continue #폴더가 아니면 continue                   
+        files = os.listdir(path+"\\"+folder)        
+        print("Foldername :",folder,"-",len(files))
+        #폴더 이름과 그 폴더에 속하는 파일 갯수 출력
+        for wav in files:
+            if not wav.endswith(".wav"):continue
+            else:               
+                print("Filename :",wav)#.wav 파일이 아니면 continue
+                y, sr = librosa.load(path+"/"+folder+"/"+wav)
+                mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13, hop_length=int(sr*0.01),n_fft=int(sr*0.02)).T
+                X_data.extend(mfcc)
+                label = [0 for i in range(len(folders))]
+                label[idx] = 1
+                for i in range(len(mfcc)):
+                    Y_label.append(label)       
+        idx = idx+1
+    #end loop
+    print("X_data :",np.shape(X_data))
+    print("Y_label :",np.shape(Y_label))
+    X_train, X_test, Y_train, Y_test = train_test_split(X_data, Y_label)
+    
+    #3d to 2d
+#     nsamples, nx, ny = np.shape(X_train)
+#     X_train = np.reshape(X_train,(nsamples,nx*ny))
+#     nsamples, nx, ny = np.shape(X_test)
+#     X_test = np.reshape(X_test,(nsamples,nx*ny))    
+    
+#     Y_train = np.argmax(Y_train, axis=1)###one-hot을 합침
+#     Y_test = np.argmax(Y_test, axis=1)###one-hot을 합침
+    xy = (X_train, X_test, Y_train, Y_test)
+    np.save("./data.npy",xy)
+    #print(X_data)
+    #print(Y_label)
+                
+load_wave_generator(DATA_PATH)
+
+#t = np.array(X_train);
+#print("!!!!!!!!",t,t.shape,X_train)
+print("X_train :",np.shape(X_train))
+print("X_test :",np.shape(X_test))
+print("Y_train :",np.shape(Y_train))
+print("Y_test :",np.shape(Y_test))
+
+clf = LogisticRegression()
+clf.fit(X_train, np.argmax(Y_train, axis=1))
+
+
+# In[46]:
+
+
+y_test_estimated = clf.predict(X_test)
+
+# 정답률 구하기 
+ac_score = metrics.accuracy_score(np.argmax(Y_test, axis=1), y_test_estimated)
+print("정답률 =", ac_score)
+print(pd.value_counts(pd.Series(y_test_estimated)))
+
+
+#y, sr = librosa.load("./youinna16.wav")
+#y, sr = librosa.load("./baecheolsu15.wav")
+y, sr = librosa.load("C:\\Users\\User\\Desktop\\바탕화면\\Downloads\\Speaker-Recognition-using-NN-master\\test_리찬희1.wav")
+
+mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13, hop_length=int(sr*0.01),n_fft=int(sr*0.02)).T
+print(mfcc.shape)
+
+y_test_estimated = clf.predict(mfcc)
+
+# 정답률 구하기 
+ac_score = metrics.accuracy_score(np.full(len(mfcc),5), y_test_estimated)
+print("정답률 =", ac_score)
+print(pd.value_counts(pd.Series(y_test_estimated)))
+
+
+# In[47]:
+
+
+import librosa
+import pyaudio #마이크를 사용하기 위한 라이브러리
+import wave
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+
+
+
+import os
+##### 변수 설정 부분 #####
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100 #비트레이트 설정
+CHUNK = int(RATE / 10) # 버퍼 사이즈 1초당 44100비트레이트 이므로 100ms단위
+RECORD_SECONDS = 5 #녹음할 시간 설정
+WAVE_OUTPUT_FILENAME = "output.wav"
+DATA_PATH = "C:\\Users\\User\\Desktop\\바탕화면\\Downloads\\Speaker-Recognition-using-NN-master\\data"
+X_train = []#train_data 저장할 공간
+X_test = []
+Y_train = []
+Y_test = []
+tf_classes = 0
+def load_wave_generator(path): 
+       
+    batch_waves = []
+    labels = []
+    X_data = []
+    Y_label = []    
+    global X_train, X_test, Y_train, Y_test, tf_classes
+    
+    folders = os.listdir(path)
+
+    for folder in folders:
+        if not os.path.isdir(path):continue #폴더가 아니면 continue                   
+        files = os.listdir(path+"/"+folder)        
+        print("Foldername :",folder,"-",len(files),"파일")
+        #폴더 이름과 그 폴더에 속하는 파일 갯수 출력
+        for wav in files:
+            if not wav.endswith(".wav"):continue
+            else:               
+                #print("Filename :",wav)#.wav 파일이 아니면 continue
+                y, sr = librosa.load(path+"/"+folder+"/"+wav)
+                mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13, hop_length=int(sr*0.01),n_fft=int(sr*0.02)).T
+              
+                X_data.extend(mfcc)
+               # print(len(mfcc))
+                
+                label = [0 for i in range(len(folders))]
+                label[tf_classes] = 1
+                
+                for i in range(len(mfcc)):
+                    Y_label.append(label)
+                #print(Y_label)
+        tf_classes = tf_classes+1
+    #end loop
+    print("X_data :",np.shape(X_data))
+    print("Y_label :",np.shape(Y_label))
+    X_train, X_test, Y_train, Y_test = train_test_split(np.array(X_data), np.array(Y_label))
+
+    xy = (X_train, X_test, Y_train, Y_test)
+    np.save("C:\\Users\\User\\Desktop\\바탕화면\\Downloads\\Speaker-Recognition-using-NN-master\\data.npy",xy)
+
+load_wave_generator(DATA_PATH)
+
+#t = np.array(X_train);
+#print("!!!!!!!!",t,t.shape,X_train)
+print(tf_classes,"개의 클래스!!")
+print("X_train :",np.shape(X_train))
+print("Y_train :",np.shape(Y_train))
+print("X_test :",np.shape(X_test))
+print("Y_test :",np.shape(Y_test))
+####################
+#clf = LogisticRegression()
+#clf.fit(X_train, Y_train)
+
+
+# In[48]:
+
+
+X_train, X_test, Y_train, Y_test = np.load(r"C:\Users\\User\\Desktop\\바탕화면\\Downloads\\Speaker-Recognition-using-NN-master\\data.npy",allow_pickle=True,encoding="ASCII")
+X_train = X_train.astype("float")
+X_test = X_test.astype("float")
+
+tf.reset_default_graph() 
+tf.set_random_seed(777)
+learning_rate = 0.001
+training_epochs = 100
+keep_prob = tf.placeholder(tf.float32)
+sd = 1 / np.sqrt(13) # standard deviation 표준편차(표본표준편차라 1/root(n))
+
+#mfcc의 기본은 20
+# 20ms일 때216은 각 mfcc feature의 열이 216
+X = tf.placeholder(tf.float32, [None, 13])
+# 
+Y = tf.placeholder(tf.float32, [None, tf_classes])
+
+# W = tf.Variable(tf.random_normal([216, 200]))
+# b = tf.Variable(tf.random_normal([200]))
+
+#1차 히든레이어
+W1 = tf.get_variable("w1",
+    #tf.random_normal([216, 180], mean=0, stddev=sd),
+        shape=[13, 256],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b1 = tf.Variable(tf.random_normal([256], mean=0, stddev=sd), name="b1")
+L1 = tf.nn.relu(tf.matmul(X, W1) + b1) # 1차 히든레이어는 'Relu' 함수를 쓴다.
+L1 = tf.nn.dropout(L1, keep_prob = keep_prob)
+
+# 2차 히든 레이어
+W2 = tf.get_variable("w2",
+    #tf.random_normal([180, 150], mean=0, stddev=sd),
+         shape=[256, 256],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b2 = tf.Variable(tf.random_normal([256], mean=0, stddev=sd), name="b2")
+L2 = tf.nn.tanh(tf.matmul(L1, W2) + b2) # 2차 히든레이어는 'Relu' 함수를 쓴다.
+L2 = tf.nn.dropout(L2, keep_prob = keep_prob)
+
+# 3차 히든 레이어
+W3 = tf.get_variable("w3",
+    #tf.random_normal([150, 100], mean=0, stddev=sd),
+            shape=[256, 256],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b3 = tf.Variable(tf.random_normal([256], mean=0, stddev=sd), name="b3")
+L3 = tf.nn.relu(tf.matmul(L2, W3) + b3) # 3차 히든레이어는 'Relu' 함수를 쓴다.
+L3 = tf.nn.dropout(L3, keep_prob = keep_prob)
+
+# 4차 히든 레이어
+W4 = tf.get_variable("w4",
+    #tf.random_normal([100, 50], mean=0, stddev=sd),
+             shape=[256, 128],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b4 = tf.Variable(tf.random_normal([128], mean=0, stddev=sd), name="b4")
+L4 = tf.nn.relu(tf.matmul(L3, W4) + b4) # 4차 히든레이어는 'Relu' 함수를 쓴다.
+L4 = tf.nn.dropout(L4, keep_prob = keep_prob)
+
+# 5차 히든 레이어
+W5 = tf.get_variable("w5",
+    #tf.random_normal([100, 50], mean=0, stddev=sd),
+             shape=[128, 128],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b5 = tf.Variable(tf.random_normal([128], mean=0, stddev=sd), name="b5")
+L5 = tf.nn.relu(tf.matmul(L4, W5) + b5) # 5차 히든레이어는 'Relu' 함수를 쓴다.
+L5 = tf.nn.dropout(L5, keep_prob = keep_prob)
+
+# 6차 히든 레이어
+W6 = tf.get_variable("w6",
+    #tf.random_normal([100, 50], mean=0, stddev=sd),
+             shape=[128, 128],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b6 = tf.Variable(tf.random_normal([128], mean=0, stddev=sd), name="b6")
+L6 = tf.nn.relu(tf.matmul(L5, W6) + b6) # 6차 히든레이어는 'Relu' 함수를 쓴다.
+L6 = tf.nn.dropout(L6, keep_prob = keep_prob)
+
+# 7차 히든 레이어
+W7 = tf.get_variable("w7",
+    #tf.random_normal([100, 50], mean=0, stddev=sd),
+             shape=[128, 128],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b7 = tf.Variable(tf.random_normal([128], mean=0, stddev=sd), name="b7")
+L7 = tf.nn.relu(tf.matmul(L6, W7) + b7) # 7차 히든레이어는 'Relu' 함수를 쓴다.
+L7 = tf.nn.dropout(L7, keep_prob = keep_prob)
+
+# 최종 레이어
+W8 = tf.get_variable("w8", 
+    #tf.random_normal([50, tf_classes], mean=0, stddev=sd),
+            shape=[128, tf_classes],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b8 = tf.Variable(tf.random_normal([tf_classes], mean=0, stddev=sd), name="b8")
+hypothesis = tf.matmul(L7, W8) + b8
+
+
+
+#cost = tf.reduce_mean(-tf.reduce_sum(Y * tf.log(hypothesis), axis=1))
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+    logits=hypothesis, labels=Y))
+
+#optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cost)
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+is_correct = tf.equal(tf.arg_max(hypothesis, 1), tf.arg_max(Y, 1))
+
+
+batch_size=1
+x_len = len(X_train)
+#짝수
+if(x_len%2==0):
+    batch_size = 2
+elif(x_len%3==0):
+    batch_size = 3
+elif(x_len%4==0):
+    batch_size = 4
+else:
+    batch_size = 1
+
+split_X = np.split(X_train,batch_size)
+split_Y = np.split(Y_train,batch_size)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+    
+for epoch in range(training_epochs):
+    avg_cost = 0
+    for i in range(batch_size):
+        batch_xs = split_X[i]
+        batch_ys = split_Y[i]
+        feed_dict = {X:batch_xs, Y:batch_ys, keep_prob: 0.7}
+        c, _ = sess.run([cost, optimizer], feed_dict=feed_dict)
+        avg_cost += c / batch_size
+        #if(epoch%10==0):
+    print('Epoch:', '%04d' % (epoch), 'cost =', '{:.9f}'.format(avg_cost))
+
+correct_prediction = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print("Accuracy: ", sess.run(accuracy, feed_dict={X: X_test, Y:Y_test, keep_prob:1}))
+
+print('Learning Finished!')
+
+
+# In[49]:
+
+
+for epoch in range(training_epochs):
+    avg_cost = 0
+    for i in range(batch_size):
+        batch_xs = split_X[i]
+        batch_ys = split_Y[i]
+        feed_dict = {X:batch_xs, Y:batch_ys, keep_prob: 0.7}
+        c, _ = sess.run([cost, optimizer], feed_dict=feed_dict)
+        avg_cost += c/batch_size
+    print('Epoch:', '%04d' %(epoch), 'cost =', '{:.9f}' .format(avg_cost))
+    
+correct_prediction = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print("Accuracy:", sess.run(accuracy, feed_dict={X: X_test, Y:Y_test, keep_prob:1}))
+
+print('Learning Finished!')
+
+
+# In[50]:
+
+
+saver = tf.train.Saver()
+saver.save(sess, './my_voice_model2')
+
+
+# In[53]:
+
+
+y, sr = librosa.load("C:\\Users\\User\\Desktop\\바탕화면\\Downloads\\Speaker-Recognition-using-NN-master\\test_리찬희1.wav")
+
+X_test = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13, hop_length=int(sr*0.01),n_fft=int(sr*0.02)).T
+'''
+0 유인나
+1 배철수
+2 이재은
+3 최일구
+4 문재인 대통령
+5 리찬희
+'''
+label = [0 for i in range(6)]
+label[5] = 1
+Y_test = []
+for i in range(len(X_test)):
+    Y_test.append(label)
+
+print(np.shape(X_test))
+print(np.shape(Y_test))
+
+correct_prediction = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print("predict")
+print(pd.value_counts(pd.Series(sess.run(tf.argmax(hypothesis, 1),
+                                         feed_dict={X: X_test, keep_prob:1}))))
+print("Accuracy: ", sess.run(accuracy, feed_dict={X: X_test, Y: Y_test, keep_prob:1}))
+
+
+# In[ ]:
+
+
+
+
